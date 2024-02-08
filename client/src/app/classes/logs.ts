d5433c276ce4430426867a738623a022a9ed8b74
ehourdebaigt <3472360-ehourdebaigt@users.noreply.gitlab.com>
2024-02-08 14:55:11 +0000

    Feature/mission view skeleton

---
+++ client/src/app/classes/logs.ts
@@ -0,0 +1,6 @@
+export interface Logs {
+  id: number;
+  robotId: number;
+  message: string;
+  timestamp: number;
+}
---
+++ client/src/app/classes/mission-data.ts
@@ -0,0 +1,6 @@
+export interface MissionData {
+  name: string;
+  distance: string;
+  elapsedTime: string;
+  status: string;
+}

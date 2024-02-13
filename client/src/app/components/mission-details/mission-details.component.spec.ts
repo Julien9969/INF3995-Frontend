21d24c19d1be77cf66211162efb72de9bfcead2b
Abdel <abdel-madjid.santanna@polymtl.ca>
2024-02-13 12:36:48 -0500

    [fix] fixed test

--- client/src/app/components/mission-details/mission-details.component.spec.ts
+++ client/src/app/components/mission-details/mission-details.component.spec.ts
@@ -1,14 +1,14 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MissionDetailsComponent} from './mission-details.component';
-
+import { HttpClientModule } from '@angular/common/http';
 describe('MissionDetailsComponent', () => {
   let component: MissionDetailsComponent;
   let fixture: ComponentFixture<MissionDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MissionDetailsComponent]
+      imports: [MissionDetailsComponent, HttpClientModule]
     })
       .compileComponents();
 

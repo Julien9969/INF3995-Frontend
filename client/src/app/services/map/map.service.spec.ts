0348ca7d647671ec55c787efbdf4c5c9da1a068d
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2024-03-19 21:08:23 -0400

    Test: map.service.ts

--- client/src/app/services/map/map.service.spec.ts
+++ client/src/app/services/map/map.service.spec.ts
@@ -1,4 +1,4 @@
-import {fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
 import {MapService} from './map.service';
 import {SocketService} from "@app/services/socket/socket.service";
@@ -9,23 +9,22 @@ describe('MapService', () => {
   let service: MapService;
   let socketClient: SocketMock;
   let socketServiceSpyObj: jasmine.SpyObj<SocketService>;
-  let imageSpyObj: jasmine.SpyObj<HTMLImageElement>;
+  let imageSubjectSpy: jasmine.Spy;
 
   beforeEach(() => {
     socketClient = new SocketMock();
     socketServiceSpyObj = jasmine.createSpyObj('SocketService', ['on'], {socketClient});
-    imageSpyObj = jasmine.createSpyObj('HTMLImageElement', ['onload']);
     const callback = (event: string, action: (Param: any) => void) => {
       action("data:image/bmp;base64,1234567890")
-    };
+    }
     socketServiceSpyObj.on.and.callFake(callback);
     TestBed.configureTestingModule({
       providers: [
         {provide: SocketService, useValue: socketServiceSpyObj},
-        {provide: HTMLImageElement, useValue: imageSpyObj}
       ]
     });
     service = TestBed.inject(MapService);
+    imageSubjectSpy = spyOn(service['imageSubject'], 'next');
   });
 
   it('should be created', () => {
@@ -57,11 +56,23 @@ describe('MapService', () => {
     expect(service['imageSubject'].getValue().src).not.toBe(imageBase64);
   });
 
-  it('should notify the observers', fakeAsync(() => {
-    const imageBase64 = "data:image/png;base64,wtv"
-    service.loadImage(imageBase64);
-    tick(100);
-    expect(service.image.getValue()).toBeInstanceOf(HTMLImageElement);
-    expect(service.image.getValue().src).toBe(imageBase64);
-  }));
+  it('should notify the observers',(done) => {
+    // Mock the Image constructor to return a mock image object
+    const mockImage = new Image();
+    spyOn(window, 'Image').and.returnValue(mockImage);
+
+    // Call the loadImage method with a valid base64 string
+    const bitmapBase64 = 'data:image/bmp;base64,...'; // Your base64 string here
+    service.loadImage(bitmapBase64);
+
+    // Manually trigger the onload event on the mock image object
+    // @ts-ignore
+    mockImage.onload(new Event('onload'));
+
+    // Use the done callback to wait for the asynchronous operation to complete
+    setTimeout(() => {
+      expect(imageSubjectSpy).toHaveBeenCalled();
+      done();
+    }, 0);
+  });
 });

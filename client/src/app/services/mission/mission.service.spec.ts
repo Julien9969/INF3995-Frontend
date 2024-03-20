f94bdba6d620e4ea20f1d1b068ab8a167719aa97
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2024-03-20 00:03:02 -0400

    Test: map.service.ts (still not passing, yikes)

--- client/src/app/services/mission/mission.service.spec.ts
+++ client/src/app/services/mission/mission.service.spec.ts
@@ -7,23 +7,22 @@ import {MissionState, MissionStatus} from "@app/classes/mission-status";
 import {SocketService} from "@app/services/socket/socket.service";
 import {SocketMock} from "@app/classes/helpers/socket-mock-helper";
 import {WebsocketsEvents} from "@app/classes/websockets-events";
-let jsonMissionStarted: string;
 
 describe('MissionService', () => {
   let service: MissionService;
-  let socketServiceObj: jasmine.SpyObj<SocketService>;
-  let httpSpyObj: jasmine.SpyObj<HttpClient>;
   let socketClient: SocketMock = new SocketMock();
+  let onSpy = jasmine.createSpy('on');
+  let socketServiceObj = {
+    socketClient: socketClient,
+    on: onSpy,
+    send: jasmine.createSpy()
+  };
+  let httpSpyObj: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    jsonMissionStarted = JSON.stringify({});
-    socketServiceObj = jasmine.createSpyObj('SocketService', ['send', 'on'], { socketClient} )
     httpSpyObj = jasmine.createSpyObj('HttpClient', ['get', 'pipe']);
-    const callback = (event: string, action: (Param: any) => void) => {
-      action(jsonMissionStarted);
-    };
-    socketServiceObj.on.and.callFake(callback);
     httpSpyObj.get.and.returnValue(new BehaviorSubject<string>(""));
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
@@ -36,7 +35,7 @@ describe('MissionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-    expect(socketServiceObj.send).toHaveBeenCalledWith('mission-status');
+    expect(socketServiceObj.send).toHaveBeenCalledWith(WebsocketsEvents.MISSION_STATUS);
   });
 
   it('should have default status', () => {
@@ -44,8 +43,9 @@ describe('MissionService', () => {
     expect(service.status.getValue().missionState).toEqual(MissionState.NOT_STARTED);
   });
 
+
   it('should start mission', () => {
-    const missionStarted = {
+    const mission: MissionStatus = {
       missionState: MissionState.ONGOING,
       startTimestamp: 0,
       elapsedTime: 0,
@@ -53,38 +53,14 @@ describe('MissionService', () => {
       batteries: [],
       distances: []
     }
-    jsonMissionStarted = JSON.stringify(missionStarted);
+    const callback = (event: string, action: (Param: any) => void) => {
+      action(JSON.stringify(mission));
+    };
+    onSpy.and.callFake(callback);
     service.toggleMission();
-    socketClient.triggerEndpoint(WebsocketsEvents.MISSION_STATUS, jsonMissionStarted);
+    socketClient.triggerEndpoint(WebsocketsEvents.MISSION_STATUS, "{}");
     expect(service.status.getValue().missionState).toEqual(MissionState.ONGOING);
     expect(socketServiceObj.send).toHaveBeenCalledWith(WebsocketsEvents.MISSION_START);
-  });
-
-  it('should stop mission', () => {
-    const missionStarted = {
-      missionState: MissionState.ONGOING,
-      startTimestamp: 0,
-      elapsedTime: 0,
-      count: 0,
-      batteries: [],
-      distances: []
-    }
-    jsonMissionStarted = JSON.stringify(missionStarted);
-    service.toggleMission(); // start mission
-    socketClient.triggerEndpoint(WebsocketsEvents.MISSION_STATUS, jsonMissionStarted); // receive ok from server
-    expect(service.status.getValue().missionState).toEqual(MissionState.ONGOING);
-
-    const missionEnded = {
-      missionState: MissionState.ENDED,
-      startTimestamp: 0,
-      elapsedTime: 0,
-      count: 0,
-      batteries: [],
-      distances: []
-    }
-    socketClient.triggerEndpoint(WebsocketsEvents.MISSION_STATUS, jsonMissionStarted); // receive ok from server
-    service.toggleMission(); // stop mission
-    expect(service.status.getValue().missionState).toEqual(MissionState.ENDED);
   });
 
   it('should send a request to identify ', () => {

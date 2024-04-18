f7ca5226d5c808f38ce37711a0e69968f01481ec
ehourdebaigt <51237571+ehourdebaigt@users.noreply.github.com>
2024-04-17 20:36:51 -0400

    Fix sorting for good now

--- client/src/app/components/logs/logs.component.ts
+++ client/src/app/components/logs/logs.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, Input, OnDestroy, ViewChild} from '@angular/core';
 import {MatDialogClose} from "@angular/material/dialog";
 import {
   MatCellDef,
@@ -40,7 +40,7 @@ import {Logs} from "@common";
   templateUrl: './logs.component.html',
   styleUrl: './logs.component.scss'
 })
-export class LogsComponent implements OnInit, OnDestroy, AfterViewInit {
+export class LogsComponent implements OnDestroy, AfterViewInit {
   @ViewChild(MatSort) sort!: MatSort;
   displayedColumns: string[] = ['eventType', 'robotId', 'message', 'timestamp']
   dataSource: MatTableDataSource<Logs> = new MatTableDataSource()
@@ -74,10 +74,6 @@ export class LogsComponent implements OnInit, OnDestroy, AfterViewInit {
       return true;
     }
     return this.activeFilters.has(data.eventType);
-  }
-
-  ngOnInit() {
-    this.subscribeToLogs();
   }
 
   ngAfterViewInit() {
